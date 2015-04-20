a925da72d4c9830c6b0bd89d023e0d219ce3c1d1
Michael Witkovsky <Michael.Witkovsky@stonybrook.edu>
2015-04-20 10:25:15 -0400

    More tweaks to the collision system (enemies now pass through player when they hit him)

--- SimpleSideScrollerFramework/src/sssf/gsm/ai/Bot.h
+++ SimpleSideScrollerFramework/src/sssf/gsm/ai/Bot.h
@@ -8,7 +8,7 @@ class Bot : public AnimatedSprite
 {
 private:
 	boolean hitPlayer = false;
-
+	boolean hitGuard = false;
 public:
 	Bot()	{}
 	~Bot()	{}
@@ -16,10 +16,16 @@ public:
 	boolean didHitPlayer(){ 
 		return hitPlayer; 
 	}
+	boolean hasHitGuard(){
+		return hitGuard;
+	}
 	void setHitPlayer(boolean hitPlayer){
 		this->hitPlayer = hitPlayer;
+		hitGuard = hitPlayer;
 	}
-
+	void setHitGuard(boolean hitGuard){
+		this->hitGuard = hitGuard;
+	}
 	// TO BE DEFINED BY BOT AI CLASSES
 	virtual void think(Game *game)=0;
 };
