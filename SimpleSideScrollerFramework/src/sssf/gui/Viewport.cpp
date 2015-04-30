0d5af690c314505e2bc127937162c2c0e0b16a92
Michael Witkovsky <michael.witkovsky@stonybrook.edu>
2015-04-30 00:16:12 -0400

    1. More correctly fixed the viewport offset issue 2. Added some dirt tiles for left and right sides of cliffs so the block doesn't stick out

--- SimpleSideScrollerFramework/src/sssf/gui/Viewport.cpp
+++ SimpleSideScrollerFramework/src/sssf/gui/Viewport.cpp
@@ -152,8 +152,8 @@ void Viewport::moveViewport(int incX,
 	// OF THE GAME WORLD. CLAMP INSTEAD.
 	// This has something to do with that gray bar I removed but since we'll never want
 	// the viewport off center this dumb solution is good enough for us, lol
-	else if (viewportY >= worldHeight - viewportHeight - 64)
-		viewportY = worldHeight - viewportHeight - 64;
+	else if (viewportY >= worldHeight - viewportHeight)
+		viewportY = worldHeight - viewportHeight;
 }
 
 
